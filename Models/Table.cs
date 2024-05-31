07cc1cc39f5a11cae4a2742483e2a755b09a08be
SrinivasDV <srinivasdv1982@gmail.com>
2024-05-31 09:30:25 +0530

    Add project files.

---
+++ Models/Table.cs
@@ -0,0 +1,21 @@
+﻿using RobotSimulator.Models;
+
+namespace RobotSimulator.Models
+{
+    public class Table
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        public Table(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public bool IsValidPosition(Position position)
+        {
+            return position.X >= 0 && position.X < Width && position.Y >= 0 && position.Y < Height;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RobotSimulator.Models;
+using RobotSimulator.Services;
+
+var table = new Table(5, 5);
+var robot = new Robot(table);
+var processor = new CommandProcessor(robot);
+
+string[] commands;
+
+if (args.Length > 0)
+{
+    commands = File.ReadAllLines(args[0]);
+}
+else
+{
+    commands = File.ReadAllLines("J:\\PracticePrograms\\RobotSimulator\\commands.txt");
+}
+
+foreach (var command in commands)
+{
+    processor.ProcessCommand(command);
+}
