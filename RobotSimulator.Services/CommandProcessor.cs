7675f69797578b25c8c82b56df6c40a53e5c9cea
SrinivasDV <srinivasdv1982@gmail.com>
2024-06-03 09:37:53 +0530

    Robot Simulator

---
+++ RobotSimulator.Services/CommandProcessor.cs
@@ -0,0 +1,44 @@
+﻿
+using RobotSimulator.Enums;
+
+namespace RobotSimulator.Services
+{
+    public class CommandProcessor
+    {
+        private readonly Robot _robot;
+
+        public CommandProcessor(Robot robot)
+        {
+            _robot = robot;
+        }
+
+        public void ProcessCommand(string command)
+        {
+            var parts = command.Split(' ');
+
+            switch (parts[0])
+            {
+                case "PLACE":
+                    var placeParams = parts[1].Split(',');
+                    int x = int.Parse(placeParams[0]);
+                    int y = int.Parse(placeParams[1]);
+                    Direction direction = Enum.Parse<Direction>(placeParams[2]);
+                    _robot.Place(x, y, direction);
+                    break;
+                case "MOVE":
+                    _robot.Move();
+                    break;
+                case "LEFT":
+                    _robot.Left();
+                    break;
+                case "RIGHT":
+                    _robot.Right();
+                    break;
+                case "REPORT":
+                    Console.WriteLine(_robot.Report());
+                    break;
+            }
+        }
+    }
+}
+
---
+++ RobotSimulator.Services/Robot.cs
@@ -0,0 +1,80 @@
+﻿using RobotSimulator.Enums;
+using RobotSimulator.Models;
+
+
+namespace RobotSimulator.Services
+{
+    public class Robot
+    {
+        private Position _position;
+        private Direction _direction;
+        private bool _isPlaced;
+        private readonly Table _table;
+
+        public Robot(Table table)
+        {
+            _table = table;
+            _isPlaced = false;
+        }
+
+        public void Place(int x, int y, Direction direction)
+        {
+            var newPosition = new Position(x, y);
+            if (_table.IsValidPosition(newPosition))
+            {
+                _position = newPosition;
+                _direction = direction;
+                _isPlaced = true;
+            }
+        }
+
+        public void Move()
+        {
+            if (!_isPlaced) return;
+
+            var newPosition = new Position(_position.X, _position.Y);
+            switch (_direction)
+            {
+                case Direction.NORTH:
+                    newPosition.Y++;
+                    break;
+                case Direction.SOUTH:
+                    newPosition.Y--;
+                    break;
+                case Direction.EAST:
+                    newPosition.X++;
+                    break;
+                case Direction.WEST:
+                    newPosition.X--;
+                    break;
+            }
+
+            if (_table.IsValidPosition(newPosition))
+            {
+                _position = newPosition;
+            }
+        }
+
+        public void Left()
+        {
+            if (!_isPlaced) return;
+
+            _direction = (Direction)(((int)_direction + 3) % 4);
+        }
+
+        public void Right()
+        {
+            if (!_isPlaced) return;
+
+            _direction = (Direction)(((int)_direction + 1) % 4);
+        }
+
+        public string Report()
+        {
+            if (!_isPlaced) return "Robot is not placed on the table.";
+
+            return $"{_position.X},{_position.Y},{_direction}";
+        }
+    }
+}
+
